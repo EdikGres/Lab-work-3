b1f8a247b83f62202116422fb85e15b5860a0f34
Ebuardo <edik_gres@icloud.com>
2020-11-09 03:28:41 +0300

    Добавьте файлы проекта.

---
+++ Lab-work-3/Main.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void zeroOut(void** mass, int element);
+
+int main() {
+	int len;
+
+	printf("input mass lenght: ");
+	scanf("%d", &len);
+	int* mass = malloc(len * sizeof(int)); //(int*)
+	
+	for (int i = 0; i < len; i++)
+	{
+		printf("input mass[%d] = ", i);
+		scanf("%d", &mass[i]);
+	}
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", mass[i]);
+	}
+	
+	zeroOut((int**)mass, 3); //(int**)
+	printf("\n");
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", mass[i]);
+	}
+	
+
+
+
+
+
+	return 0;
+}
+
+
+void zeroOut(void** mass, int element) {  //по идее если написать элемент больший, чем длина массива, ничего не будет, если память в хипе далее ничем не забита
+	printf("\n size: %d", sizeof(mass));
+	(mass[element]) = 0; // приведение для с++ (int**)
+
+}
